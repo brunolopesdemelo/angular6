118891715e4631eb0f01070fbe9f78d0682a6d26
Bruno Melo <bruno.melo@basis.com.br>
2022-11-02 14:17:28 -0300

    add routing

--- README.md
+++ README.md
@@ -25,3 +25,18 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+
+## Create modules
+ng generate module [NAME-MODULE] --routing
+## Create modules
+ng generate component [NAME-MODULE]/[NAME-COMPONENT]
+
+## add routings
+app-routing.module.ts
+
+const routes: Routes = [  {  path: 'home', loadChildren: './home/home.module#Module' } ];
+
+[NAME-MODULE]-routing.module.ts
+
+const routes: Routes = [ { path: '', component: Component } ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -12,8 +13,7 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatCheckboxModule
-
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
