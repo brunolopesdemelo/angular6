36ccf595fb0d7ab4e9e8a0664828fafd104ee2f0
Bruno Melo <bruno.melo@basis.com.br>
2022-11-02 00:45:30 -0300

    fix rxjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCheckboxModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
